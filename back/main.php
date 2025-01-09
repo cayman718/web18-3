04bbabd066fc1e8cc79845d2e883f67e9defef13
cayman718 <a44362652@gmail.com>
2025-01-09 16:36:14 +0800

    建立後端相關

--- back/main.php
+++ back/main.php
@@ -1,3 +1,40 @@
+<?php
+if (isset($_POST['acc'])) {
+    if ($_POST['acc'] == 'admin' && $_POST['pw'] == '1234') {
+        $_SESSION['login'] = 1;
+    } else {
+        echo "帳密錯誤";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+if (!isset($_SESSION['login'])) {
+?>
+<form action="?" method="post"></form>
+<table></table>
+
+
+
+
+<?php
+} else {
+?>
+
 <div class="rb tab">
     <h2 class="ct">請選擇所需功能</h2>
 </div>
+
+<?php
+}
+?>
+</form>
