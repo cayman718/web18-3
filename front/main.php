cae4c5a4d2e28d42a73785829606eb27d393903d
cayman718 <a44362652@gmail.com>
2025-01-13 10:31:19 +0800

    建立滑鼠移入按鈕區時會停止輪播的機制

--- front/main.php
+++ front/main.php
@@ -1,88 +1,88 @@
 <style>
-.poster-block * {
-    margin: 0;
-    padding: 0;
-    font-size: 12px;
-    box-sizing: border-box;
-}
+    .poster-block * {
+        margin: 0;
+        padding: 0;
+        font-size: 12px;
+        box-sizing: border-box;
+    }
 
-.poster-block {
-    width: 420px;
-    height: 400px;
-}
+    .poster-block {
+        width: 420px;
+        height: 400px;
+    }
 
-.lists {
-    width: 210px;
-    height: 280px;
-    margin: auto;
-    position: relative;
-}
+    .lists {
+        width: 210px;
+        height: 280px;
+        margin: auto;
+        position: relative;
+    }
 
-.controls {
-    width: 100%;
-    height: 100px;
-    margin: 10px auto;
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-}
+    .controls {
+        width: 100%;
+        height: 100px;
+        margin: 10px auto;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+    }
 
-.poster {
-    position: absolute;
-    display: none;
-    text-align: center;
-}
+    .poster {
+        position: absolute;
+        display: none;
+        text-align: center;
+    }
 
-.poster img {
-    display: block;
-    width: 210px;
-    height: 250px;
-}
+    .poster img {
+        display: block;
+        width: 210px;
+        height: 250px;
+    }
 
-.poster span {
-    font-size: 18px;
-}
+    .poster span {
+        font-size: 18px;
+    }
 
-.left,
-.right {
-    width: 0;
-    border-top: 15px solid transparent;
-    border-bottom: 15px solid transparent;
-}
+    .left,
+    .right {
+        width: 0;
+        border-top: 15px solid transparent;
+        border-bottom: 15px solid transparent;
+    }
 
-.left {
-    border-right: 25px solid #eee;
-    border-left: 0;
-}
+    .left {
+        border-right: 25px solid #eee;
+        border-left: 0;
+    }
 
-.right {
-    border-left: 25px solid #eee;
-    border-right: 0;
-}
+    .right {
+        border-left: 25px solid #eee;
+        border-right: 0;
+    }
 
-.icons {
-    width: 320px;
-    display: flex;
-    overflow: hidden;
-    position: relative;
-}
+    .icons {
+        width: 320px;
+        display: flex;
+        overflow: hidden;
+        position: relative;
+    }
 
-.icon {
-    width: 80px;
-    height: 100px;
-    flex-shrink: 0;
-    text-align: center;
-    position: relative;
-}
+    .icon {
+        width: 80px;
+        height: 100px;
+        flex-shrink: 0;
+        text-align: center;
+        position: relative;
+    }
 
-.icon img {
-    width: 70px;
-    height: 80px;
-}
+    .icon img {
+        width: 70px;
+        height: 80px;
+    }
 
-.icon div {
-    font-size: 12px;
-}
+    .icon div {
+        font-size: 12px;
+    }
 </style>
 <div class="half" style="vertical-align:top;">
     <h1>預告片介紹</h1>
@@ -93,10 +93,10 @@
                 $posters = $Poster->all(['sh' => 1], " order by rank");
                 foreach ($posters as $idx => $poster):
                 ?>
-                <div class="poster" data-ani="<?= $poster['ani']; ?>">
-                    <img src="./upload/<?= $poster['img']; ?>" alt="">
-                    <span><?= $poster['name']; ?></span>
-                </div>
+                    <div class="poster" data-ani="<?= $poster['ani']; ?>">
+                        <img src="./upload/<?= $poster['img']; ?>" alt="">
+                        <span><?= $poster['name']; ?></span>
+                    </div>
                 <?php
                 endforeach;
                 ?>
@@ -107,10 +107,10 @@
                     <?php
                     foreach ($posters as $idx => $poster):
                     ?>
-                    <div class="icon">
-                        <img src="./upload/<?= $poster['img']; ?>">
-                        <div><?= $poster['name']; ?></div>
-                    </div>
+                        <div class="icon">
+                            <img src="./upload/<?= $poster['img']; ?>">
+                            <div><?= $poster['name']; ?></div>
+                        </div>
                     <?php endforeach; ?>
                 </div>
                 <div class='right'></div>
@@ -120,41 +120,68 @@
 </div>
 
 <script>
-$(".poster").eq(0).show();
+    $(".poster").eq(0).show();
 
-let slider = setInterval(() => {
-    sliders();
-}, 2500);
+    let slider = setInterval(() => {
+        sliders();
+    }, 2500);
 
-function sliders() {
-    let now = $(".poster:visible").index();
-    let next = ($(".poster").length == now + 1) ? 0 : now + 1;
-    let ani = $(".poster").eq(next).data('ani');
-    //console.log(now,next)
+    function sliders() {
+        let now = $(".poster:visible").index();
+        let next = ($(".poster").length == now + 1) ? 0 : now + 1;
+        let ani = $(".poster").eq(next).data('ani');
+        //console.log(now,next)
 
-    switch (ani) {
-        case 1:
-            //淡入淡出
-            $(".poster").eq(now).fadeOut(1000, function() {
-                $(".poster").eq(next).fadeIn(1000);
-            });
-            break;
-        case 2:
-            //縮放
-            $(".poster").eq(now).hide(1000, function() {
-                $(".poster").eq(next).show(1000);
-            });
-            break;
-        case 3:
-            //滑入滑出
-            $(".poster").eq(now).slideUp(1000, function() {
-                $(".poster").eq(next).slideDown(1000);
-            });
+        switch (ani) {
+            case 1:
+                //淡入淡出
+                $(".poster").eq(now).fadeOut(1000, function() {
+                    $(".poster").eq(next).fadeIn(1000);
+                });
+                break;
+            case 2:
+                //縮放
+                $(".poster").eq(now).hide(1000, function() {
+                    $(".poster").eq(next).show(1000);
+                });
+                break;
+            case 3:
+                //滑入滑出
+                $(".poster").eq(now).slideUp(1000, function() {
+                    $(".poster").eq(next).slideDown(1000);
+                });
 
-            break;
+                break;
+        }
+
     }
+    let total = $(".icon").length;
+    let p = 0;
+    $(".left,.right").on("click", function() {
+        if ($(this).hasClass('left')) {
+            if (p - 1 >= 0) {
+                p--;
+            }
+        } else {
+            p = (p + 1 <= total - 4) ? p + 1 : total - 4;
+        }
+        // if ((p + 1) <= (total - 4)) {
+        //     p++;
 
-}
+        // }
+        $(".icon").animate({
+            right: 80 * p
+        });
+    })
+
+    $(".icons").hover(function() {
+            clearInterval(slider);
+        },
+        function() {
+            slider = setInterval(() => {
+                sliders();
+            }, 2500);
+        })
 </script>
 
 
@@ -175,44 +202,44 @@ function sliders() {
 
     ?>
     <style>
-    .movie-item {
-        width: 49%;
-        height: 150px;
-        border: 1px solid #ccc;
-        border-radius: 5px;
-        margin: 0.25%;
-        display: flex;
-        flex-wrap: wrap;
-        padding: 3px;
-        box-sizing: border-box;
-        font-size: 14px;
-        align-content: center;
-    }
+        .movie-item {
+            width: 49%;
+            height: 150px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            margin: 0.25%;
+            display: flex;
+            flex-wrap: wrap;
+            padding: 3px;
+            box-sizing: border-box;
+            font-size: 14px;
+            align-content: center;
+        }
     </style>
     <div class="rb tab" style="width:95%;">
         <div style="display:flex;flex-wrap:wrap">
             <?php
             foreach ($rows as $row):
             ?>
-            <div class='movie-item'>
-                <div style="width:65px;">
-                    <a href="?do=intro&id=<?= $row['id']; ?>">
-                        <img src="./upload/<?= $row['poster']; ?>" style="width:60px;height:80px;">
-                    </a>
-                </div>
-                <div style="width:calc(100% - 65px);">
-                    <div style="font-size:18px;"><?= $row['name']; ?></div>
-                    <div>分級:
-                        <img src="./icon/03C0<?= $row['level']; ?>.png" style="width:20px;vertical-align:middle">
-                        <?= $Movie::$level[$row['level']]; ?>
+                <div class='movie-item'>
+                    <div style="width:65px;">
+                        <a href="?do=intro&id=<?= $row['id']; ?>">
+                            <img src="./upload/<?= $row['poster']; ?>" style="width:60px;height:80px;">
+                        </a>
                     </div>
-                    <div>上映日期:<?= $row['ondate']; ?></div>
+                    <div style="width:calc(100% - 65px);">
+                        <div style="font-size:18px;"><?= $row['name']; ?></div>
+                        <div>分級:
+                            <img src="./icon/03C0<?= $row['level']; ?>.png" style="width:20px;vertical-align:middle">
+                            <?= $Movie::$level[$row['level']]; ?>
+                        </div>
+                        <div>上映日期:<?= $row['ondate']; ?></div>
+                    </div>
+                    <div style="width:100%;" class="ct">
+                        <button onclick="location.href='?do=intro&id=<?= $row['id']; ?>'">劇情簡介</button>
+                        <button onclick="location.href='?do=order&id=<?= $row['id']; ?>'">線上訂票</button>
+                    </div>
                 </div>
-                <div style="width:100%;" class="ct">
-                    <button onclick="location.href='?do=intro&id=<?= $row['id']; ?>'">劇情簡介</button>
-                    <button onclick="location.href='?do=order&id=<?= $row['id']; ?>'">線上訂票</button>
-                </div>
-            </div>
             <?php
             endforeach;
             ?>
